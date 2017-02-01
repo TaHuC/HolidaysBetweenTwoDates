3075fd2ce083be39b82e61762f5f304656eabe1c
TaHuC <TaHuC@hotmail.com>
2017-02-01 09:06:50 +0200

    Finish editing

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace HolidaysBetweenTwoDates
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+
+
+			var startDate = DateTime.ParseExact(Console.ReadLine(), 
+			                                    "d.M.yyyy", 
+			                                    CultureInfo.InvariantCulture);
+			var endDate = DateTime.ParseExact(Console.ReadLine(), 
+			                                  "d.M.yyyy",
+			                                  CultureInfo.InvariantCulture);
+
+			var holidaysCount = 0;
+
+
+			for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+			{
+				if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+				{
+					holidaysCount++;
+				}
+			}
+
+			Console.WriteLine(holidaysCount);
+
+		}
+	}
+}
